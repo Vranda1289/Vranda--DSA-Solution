86ec1d963a0648c1ec1a1a21cf0fbc0e5be46e89
Vranda1289 <wadhvavranda@gmail.com>
2026-02-08 21:05:57 +0530

    Time: 8 ms (49.73%), Space: 58.5 MB (5%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> result = new ArrayList<>();
+
+        int n = nums.length;
+        int count = 1;
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            } else {
+                if(count > n/3)
+                    result.add(nums[i-1]);
+                count = 1;
+            }
+        }
+
+        if(count > n/3)
+            result.add(nums[n-1]);
+
+        return result;
+    }
+}
