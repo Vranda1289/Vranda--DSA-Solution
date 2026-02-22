644f7bbd28a46f85d65fefa382bbeafb28e1f74f
Vranda1289 <wadhvavranda@gmail.com>
2026-02-22 16:34:15 +0530

    Time: 22 ms (92.18%), Space: 91 MB (10.06%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer > map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0; i<n; i++){
+            if(map.containsKey(nums[i])){
+                int prevIndex=map.get(nums[i]);
+
+                if(i-prevIndex<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i], i); 
+        }
+        return false ;
+    }
+}
