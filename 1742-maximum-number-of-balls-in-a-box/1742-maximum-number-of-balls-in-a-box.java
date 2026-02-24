7239af35f2b6057817e8d8f9a5dff305e4890931
Vranda1289 <wadhvavranda@gmail.com>
2026-02-24 12:10:01 +0530

    Time: 93 ms (7.42%), Space: 46.1 MB (30.98%) - LeetHub

---
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countBalls(int lowLimit, int highLimit) {
+        HashMap <Integer,Integer> map=new HashMap<>();
+        int max=0;
+        for(int i=lowLimit; i<=highLimit; i++){
+             int sum=0;
+             int num=i;
+        while(num>0){
+                int digit=num%10;
+                sum+=digit;
+                num /=10;
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+            max = Math.max(max,map.get(sum));
+        }
+        return max;
+    }
+}
