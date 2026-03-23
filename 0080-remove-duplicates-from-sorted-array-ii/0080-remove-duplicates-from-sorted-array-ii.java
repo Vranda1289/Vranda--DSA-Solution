57b635901d01ead594e7bd9c9325bcf29eee91aa
Vranda1289 <vranda1289@gmail.com>
2026-03-23 19:08:34 +0530

    Time: 4 ms (6.04%), Space: 47.9 MB (98.06%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,15 +1,32 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if (n <= 2) return n;
+        // int n = nums.length;
+        // if (n <= 2) return n;
 
-        int rd = 2;
-        for (int i = 2; i < n; i++) {
-            if (nums[i] != nums[rd - 2]) {
-                nums[rd] = nums[i];
-                rd++;
+        // int rd = 2;
+        // for (int i = 2; i < n; i++) {
+        //     if (nums[i] != nums[rd - 2]) {
+        //         nums[rd] = nums[i];
+        //         rd++;
+        //     }
+        // }
+        // return rd;
+
+        List <Integer> list = new ArrayList<>();
+        HashMap <Integer,Integer> map=new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int freq=map.getOrDefault(nums[i],0);
+            if(freq<2){
+                list.add(nums[i]);
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             }
         }
-        return rd;
+        int k=0;
+        for(int i=0; i<list.size(); i++){
+            nums[i]=list.get(k);
+            k++;
+        }
+        return k;
     }
 }
