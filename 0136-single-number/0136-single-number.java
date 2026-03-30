0e86b54325a091cca9436300b952fa2eb4547e74
Vranda1289 <vranda1289@gmail.com>
2026-03-30 11:43:47 +0530

    Time: 1 ms (99.98%), Space: 47 MB (38.24%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,13 +1,9 @@
 class Solution {
-
-    public int find(int[] nums,int index) {
-        if(index==nums.length){
-            return 0;
-        }
-        return nums[index] ^ find(nums,index+1);
-    }
-
     public int singleNumber(int[] nums) {
-        return find(nums,0);
+        int res=0;
+        for(int i=0; i<nums.length; i++){
+           res=res^nums[i];
+        }
+        return res;
     }
 }
