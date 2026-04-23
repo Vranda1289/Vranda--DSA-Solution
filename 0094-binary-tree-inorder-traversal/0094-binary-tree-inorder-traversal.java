093713c366fe518831a345ca8eb341be51ecb979
Vranda1289 <vranda1289@gmail.com>
2026-04-23 11:15:18 +0530

    Time: 1 ms (6.15%), Space: 43.3 MB (34.05%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,17 +14,36 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode root, ArrayList<Integer> list){
-        if(root==null){
-            return ;
-        }
-        helper(root.left,list);
-        list.add(root.val);
-        helper(root.right,list);
-    }
+    // public void helper(TreeNode root, ArrayList<Integer> list){
+    //     if(root==null){
+    //         return ;
+    //     }
+    //     helper(root.left,list);
+    //     list.add(root.val);
+    //     helper(root.right,list);
+    // }
     public List<Integer> inorderTraversal(TreeNode root) {
+        // ArrayList<Integer> list=new ArrayList<>();
+        // helper(root,list);
+        // return list;
         ArrayList<Integer> list=new ArrayList<>();
-        helper(root,list);
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode node=root;
+
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node=st.pop();
+                list.add(node.val);
+                node=node.right;
+            }
+        }
         return list;
     }
 }
