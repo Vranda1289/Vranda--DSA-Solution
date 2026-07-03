8349f160ad7902d05ef33365ac47b346eede7194
Vranda1289 <vranda1289@gmail.com>
2026-07-03 16:47:15 +0530

    Time: 5 ms (89.12%), Space: 48.5 MB (8.99%) - LeetHub

---
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double minimumAverage(int[] nums) {
+        Arrays.sort(nums);
+
+        int n=nums.length;
+
+        int i=0;
+        int j=n-1;
+
+        double ans=Double.MAX_VALUE;
+
+        while(i<j){
+            double avg=(nums[i]+nums[j])/2.0;
+            ans=Math.min(ans,avg);
+
+            i++;
+            j--;
+        }
+        return ans;
+    }
+}
